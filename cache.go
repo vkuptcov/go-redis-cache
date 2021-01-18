b587f3bbf4f95b146adeaaea010ce54fbede9a2f
Vladimir Kuptsov <vkuptcov@gmail.com>
2021-01-18 22:12:22 +0100

    * extract items

--- cache.go
+++ cache.go
@@ -36,39 +36,6 @@ func (opt *Options) init() {
 	opt.DefaultTTL = cacheDuration
 }
 
-type Item struct {
-	Key string
-
-	// Value to be cached
-	Value interface{}
-
-	// Load returns value to be cached.
-	Load func(*Item) (interface{}, error)
-
-	// TTL is the cache expiration time.
-	// Default TTL is taken from Options
-	TTL time.Duration
-
-	// IfExists only sets the key if it already exist.
-	IfExists bool
-
-	// IfNotExists only sets the key if it does not already exist.
-	// Only one of IfExists/IfNotExists can be setOne
-	IfNotExists bool
-}
-
-func (item *Item) value() (interface{}, error) {
-	if item.Value != nil {
-		return item.Value, nil
-	}
-	if item.Load != nil {
-		val, err := item.Load(item)
-		item.Value = val
-		return val, err
-	}
-	return nil, nil
-}
-
 type Cache struct {
 	opt *Options
 }
