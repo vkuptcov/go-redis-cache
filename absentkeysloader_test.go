5c9e1e39ef66751ea059f1b03473bb9beee71e5c
Vladimir Kuptsov <vkuptcov@gmail.com>
2021-03-08 23:35:39 +0100

    + more tests for WithAbsentKeysLoader modifier for a single element

--- absentkeysloader_test.go
+++ absentkeysloader_test.go
@@ -14,20 +14,49 @@ type CacheAbsentKeysLoaderSuite struct {
 func (st *CacheAbsentKeysLoaderSuite) TestReturningSingleElement() {
 	keyToLoad := faker.RandomString(5)
 
-	var dst string
-	st.Require().NoError(
-		st.cache.WithAbsentKeysLoader(func(absentKeys ...string) (interface{}, error) {
-			st.Require().Len(absentKeys, 1, "Only one key should be passed here")
-			return absentKeys[0] + "-element", nil
-		}).Get(
-			st.ctx,
-			&dst,
-			keyToLoad,
-		),
-		"No error expected for loading with absent keys",
-	)
+	keyToElement := func(k string) string {
+		return k + "-element"
+	}
 
-	st.Require().Equal(keyToLoad+"-element", dst, "unexpected loaded item")
+	testCases := []struct {
+		testCase string
+		dst      interface{}
+		expected interface{}
+	}{
+		{
+			testCase: "load into a string",
+			dst:      "",
+			expected: keyToElement(keyToLoad),
+		},
+		{
+			testCase: "load into a slice",
+			dst:      []string{},
+			expected: []string{keyToElement(keyToLoad)},
+		},
+		{
+			testCase: "load into a map",
+			dst:      map[string]string{},
+			expected: map[string]string{keyToLoad: keyToElement(keyToLoad)},
+		},
+	}
+	for _, tc := range testCases {
+		st.Run(tc.testCase, func() {
+			dst := tc.dst
+			st.Require().NoError(
+				st.cache.WithAbsentKeysLoader(func(absentKeys ...string) (interface{}, error) {
+					st.Require().Len(absentKeys, 1, "Only one key should be passed here")
+					return keyToElement(absentKeys[0]), nil
+				}).Get(
+					st.ctx,
+					&dst,
+					keyToLoad,
+				),
+				"No error expected for loading with absent keys",
+			)
+
+			st.Require().EqualValues(tc.expected, dst, "unexpected loaded item")
+		})
+	}
 }
 
 func TestCacheAbsentKeysLoaderSuite(t *testing.T) {
