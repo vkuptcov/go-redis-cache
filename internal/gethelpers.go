3adce3c32e8702c88f6201099233b8ea833a461b
Vladimir Kuptsov <vkuptcov@gmail.com>
2021-04-06 23:11:46 +0200

    + extract dedicated handle redis commands functions

--- internal/gethelpers.go
+++ internal/gethelpers.go
@@ -9,7 +9,6 @@ import (
 	"github.com/vkuptcov/go-redis-cache/v8/internal/containers"
 )
 
-//nolint:gocognit,gocyclo // @todo move switch cases in different functions
 func execAndAddIntoContainer(ctx context.Context, opts Options, dst interface{}, pipelinerFiller func(pipeliner redis.Pipeliner)) error {
 	if opts.AbsentKeysLoader != nil {
 		opts.AddCacheMissErrors = true
@@ -47,41 +46,12 @@ func execAndAddIntoContainer(ctx context.Context, opts Options, dst interface{},
 		switch typedCmd := cmderr.(type) {
 		// returned for HMGET
 		case *redis.SliceCmd:
-			fields := typedCmd.Args()[2:]
-			for fieldIdx, val := range typedCmd.Val() {
-				field := fields[fieldIdx].(string)
-				switch t := val.(type) {
-				case error:
-					if !errors.Is(t, redis.Nil) {
-						return t
-					}
-				case string:
-					decodeErr := decodeAndAddElementToContainer(opts, container, key, field, t)
-					if decodeErr != nil {
-						byKeysErr.AddErrorForKeyAndField(key, field, decodeErr)
-					}
-				default:
-					if t == nil {
-						if opts.AddCacheMissErrors {
-							byKeysErr.AddErrorForKeyAndField(key, field, errors.Wrapf(ErrCacheMiss, "key %q and field %q not found", key, field))
-						}
-						continue
-					}
-					return errors.Errorf("Non-handled type returned: %T", t)
-				}
+			if handleErr := handleSliceCmd(typedCmd, opts, container, key, byKeysErr); handleErr != nil {
+				return handleErr
 			}
 		// returned for HGETALL
 		case *redis.StringStringMapCmd:
-			for field, val := range typedCmd.Val() {
-				decodeErr := decodeAndAddElementToContainer(opts, container, key, field, val)
-				if decodeErr != nil {
-					byKeysErr.AddErrorForKeyAndField(key, field, decodeErr)
-				}
-			}
-			// HGETALL doesn't return redis.Nil error for absent keys and returns just an empty list
-			if len(typedCmd.Val()) == 0 {
-				byKeysErr.AddErrorForKey(key, errors.Wrapf(ErrCacheMiss, "key %q not found", key))
-			}
+			handleStringStringMapCmd(typedCmd, opts, container, key, byKeysErr)
 		case *redis.StringCmd:
 			decodeErr := decodeAndAddElementToContainer(opts, container, key, "", typedCmd.Val())
 			if decodeErr != nil {
@@ -91,6 +61,46 @@ func execAndAddIntoContainer(ctx context.Context, opts Options, dst interface{},
 	}
 	if len(byKeysErr.KeysToErrs) > 0 {
 		return byKeysErr
+	}
+	return nil
+}
+
+func handleStringStringMapCmd(typedCmd *redis.StringStringMapCmd, opts Options, container containers.Container, key string, byKeysErr *KeyErr) {
+	for field, val := range typedCmd.Val() {
+		decodeErr := decodeAndAddElementToContainer(opts, container, key, field, val)
+		if decodeErr != nil {
+			byKeysErr.AddErrorForKeyAndField(key, field, decodeErr)
+		}
+	}
+	// HGETALL doesn't return redis.Nil error for absent keys and returns just an empty list
+	if len(typedCmd.Val()) == 0 {
+		byKeysErr.AddErrorForKey(key, errors.Wrapf(ErrCacheMiss, "key %q not found", key))
+	}
+}
+
+func handleSliceCmd(typedCmd *redis.SliceCmd, opts Options, container containers.Container, key string, byKeysErr *KeyErr) error {
+	fields := typedCmd.Args()[2:]
+	for fieldIdx, val := range typedCmd.Val() {
+		field := fields[fieldIdx].(string)
+		switch t := val.(type) {
+		case error:
+			if !errors.Is(t, redis.Nil) {
+				return t
+			}
+		case string:
+			decodeErr := decodeAndAddElementToContainer(opts, container, key, field, t)
+			if decodeErr != nil {
+				byKeysErr.AddErrorForKeyAndField(key, field, decodeErr)
+			}
+		default:
+			if t == nil {
+				if opts.AddCacheMissErrors {
+					byKeysErr.AddErrorForKeyAndField(key, field, errors.Wrapf(ErrCacheMiss, "key %q and field %q not found", key, field))
+				}
+				continue
+			}
+			return errors.Errorf("Non-handled type returned: %T", t)
+		}
 	}
 	return nil
 }
