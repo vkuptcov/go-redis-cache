92e1d41c6928ce44f7bf2191eee436ca085d9138
Vladimir Kuptsov <vkuptcov@gmail.com>
2021-05-18 22:56:38 +0200

    + save and get test

--- examples/example_saveget_test.go
+++ examples/example_saveget_test.go
@@ -3,6 +3,7 @@ package examples
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/go-redis/redis/v8"
 	cache "github.com/vkuptcov/go-redis-cache/v8"
@@ -36,6 +37,8 @@ func Example_saveAndGetUser() {
 	keyByID := cachekeys.CreateKey("usr", user.ID)
 	keyByDepartment := cachekeys.CreateKey("usr-by-department", user.Department)
 
+	// store user with a key derived for their id
+	// store the same user with a key derived from department
 	saveErr := cacheInst.Set(
 		context.Background(),
 		&cache.Item{
@@ -54,6 +57,7 @@ func Example_saveAndGetUser() {
 
 	var loadedUserByID User
 	var loadedUserByDepartment User
+	// get user by id
 	loadByIDErr := cacheInst.Get(
 		context.Background(),
 		&loadedUserByID,
@@ -63,6 +67,7 @@ func Example_saveAndGetUser() {
 		panic(loadByIDErr)
 	}
 
+	// get user by department
 	loadByDepartmentErr := cacheInst.HGetFieldsForKey(
 		context.Background(),
 		&loadedUserByDepartment,
@@ -75,7 +80,70 @@ func Example_saveAndGetUser() {
 
 	fmt.Println(loadedUserByID)
 	fmt.Println(loadedUserByDepartment)
+	fmt.Println(loadedUserByDepartment == loadedUserByID)
 
 	// Output: {u-1 FirstUserName R&D}
 	// {u-1 FirstUserName R&D}
+	// true
+}
+
+func Example_saveSeveralUsersAndLoadInSlice() {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	cacheInst := cache.NewCache(cache.Options{
+		Redis:      client,
+		Marshaller: marshaller.NewMarshaller(&marshallers.JSONMarshaller{}),
+	})
+
+	keyByID := func(id string) string {
+		return cachekeys.CreateKey("usr", id)
+	}
+
+	firstUser := &User{
+		ID:         "u-1",
+		Name:       "FirstUserName",
+		Department: "R&D",
+	}
+	secondUser := &User{
+		ID:         "u-2",
+		Name:       "SecondUserName",
+		Department: "IT",
+	}
+
+	saveErr := cacheInst.Set(
+		context.Background(),
+		&cache.Item{
+			Key:   keyByID(firstUser.ID),
+			Value: firstUser,
+		},
+		&cache.Item{
+			Key:   keyByID(secondUser.ID),
+			Value: secondUser,
+		},
+	)
+	if saveErr != nil {
+		panic(saveErr)
+	}
+
+	var loadedUsers []*User
+
+	loadErr := cacheInst.Get(
+		context.Background(),
+		&loadedUsers,
+		keyByID(firstUser.ID),
+		keyByID(secondUser.ID),
+	)
+	if loadErr != nil {
+		panic(loadErr)
+	}
+	sort.Slice(loadedUsers, func(i, j int) bool {
+		return loadedUsers[i].ID < loadedUsers[j].ID
+	})
+	for _, u := range loadedUsers {
+		fmt.Println(u)
+	}
+	// Output:&{u-1 FirstUserName R&D}
+	// &{u-2 SecondUserName IT}
 }
